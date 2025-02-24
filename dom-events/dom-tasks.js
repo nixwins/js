9b2825511cd25486c5b6b166e451df7225c453a2
Dos <halaulilau@gmail.com>
2025-02-24 18:25:08 +0500

    tasks

---
+++ dom-events/dom-tasks.js
@@ -0,0 +1,27 @@
+const images = [
+  {
+    id: 1,
+    url: "https://picsum.photos/200/300",
+  },
+  {
+    id: 2,
+    url: "https://picsum.photos/200/300",
+  },
+  {
+    id: 3,
+    url: "https://picsum.photos/200/300",
+  },
+];
+
+// 1.  Создать новый элемент изображения
+// 2. Добавить изображение в DOM
+// 3. images сколько есть элементов столько img добавить в DOM img.src = images[i].url
+// 4. Добавить две кнопки next и prev
+// 5. Когда нажимаешь на кнопку next images[1] изменить display: none;
+// 6. Когда нажимаешь на кнопку prev images[1] изменить display: block;
+// 7. Для всех images добавить display: none; кроме  i === 0
+
+//  Создать слайдер (slider.html)
+// Одна кнопка next
+//  Вывести  все изображения из images и у всех display: none кроме первого;
+// Когда нажимаешь на кнопку next текущуу изображение изменить display: none; следующее изображение изменить display: block;
