d5ee0eaaa9fc851a1fb91d9524accaab5fb16ac1
Burning <happyhaccker@gmail.com>
2025-02-24 19:53:28 +0500

    dom-task

--- dom-events/dom-tasks.js
+++ dom-events/dom-tasks.js
@@ -13,14 +13,59 @@ const images = [
   },
 ];
 
-// 1.  Создать новый элемент изображения
-// 2. Добавить изображение в DOM
-// 3. images сколько есть элементов столько img добавить в DOM img.src = images[i].url
-// 4. Добавить две кнопки next и prev
-// 5. Когда нажимаешь на кнопку next images[1] изменить display: none;
-// 6. Когда нажимаешь на кнопку prev images[1] изменить display: block;
-// 7. Для всех images добавить display: none; кроме  i === 0
+console.log(images);
 
+
+const bodyEl = document.querySelector("body");
+
+
+
+// 1.  Создать новый элемент изображения
+const imgEl = document.createElement("img");
+
+// 2. Добавить изображение в DOM
+bodyEl.append(imgEl);
+
+// 3. images сколько есть элементов столько img добавить в DOM img.src = images[i].url
+const imgElement = [];
+
+for (let i = 0; i < images.length; i++) {
+  let imgElem = document.createElement("img");
+    imgElement.push(imgElem)
+    imgElem.src = images[i].url;
+    bodyEl.append(imgElem);
+    
+}
+
+// 4. Добавить две кнопки next и prev
+const nextButton = document.createElement("button");
+nextButton.innerText = "next";
+
+const prevButton = document.createElement("button");
+prevButton.innerText = "prev";
+
+prevButton.classList.add("button");
+nextButton.classList.add("button");
+
+bodyEl.append(prevButton, nextButton);
+// 5. Когда нажимаешь на кнопку next images[1] изменить display: none;
+console.log(imgElement);
+
+
+nextButton.onclick = function () {
+imgElement[1].style.display = "none";
+   }
+// 6. Когда нажимаешь на кнопку prev images[1] изменить display: block;
+
+prevButton.onclick = function () {
+  imgElement[1].style.display = "block";
+}
+// 7. Для всех images добавить display: none; кроме  i === 0
+  for (let i = 0; i < imgElement.length; i++) {
+    if (i === 0) {
+      imgElement[i].style.display = "none";
+  }
+}
 //  Создать слайдер (slider.html)
 // Одна кнопка next
 //  Вывести  все изображения из images и у всех display: none кроме первого;
