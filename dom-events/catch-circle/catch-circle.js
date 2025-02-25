0d630280f447ce34af9d3cc6c0db1eb736a7d10d
Dos <halaulilau@gmail.com>
2025-02-25 23:24:14 +0500

    new task

---
+++ dom-events/catch-circle/catch-circle.js
@@ -0,0 +1,13 @@
+// 1. Получить div circle
+// 2 Задать Position circle absolute
+// 3  Здать кругу координаты посмтерть circle.style.left = 0px circle.style.top = 0px
+//4 Здать кругу координаты посмтерть circle.style.left = 100px circle.style.top = 100px
+
+//5 изучить Math.random()
+// 6 Создать функцию которая будет возращать рандоные цифры от 0 до 800 https://learn.javascript.ru/task/random-int-min-max посмотерть и изучить решение
+// 7  Получить рондомные число из фукнции 6 задачи и присвоить circle.style.left  circle.style.top
+
+// 8 Добавить событие click  на circle если пользователь кликнул на круг то вывести в консоль нажата и окраисть круг в красный цвет
+
+//9 setInterval изучить
+// 10  через setInterval менять координаты круга вызывать функцию 7 задачи и менять координаты круга  left top
