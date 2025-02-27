710ee51786aa003be8d013ee9e212f1f095806bc
Dos <halaulilau@gmail.com>
2025-02-27 17:36:09 +0500

    event

--- dom-events/form-tasks.js
+++ dom-events/form-tasks.js
@@ -9,32 +9,39 @@ const userPass = document.querySelector("input[name='password']");
 const outUsername = document.querySelector(".out-username");
 const outPassword = document.querySelector(".out-password");
 
-
-
 const btn = document.querySelector("#form button");
 
-btn.onclick = function (){
-    console.log("pirvet");
-    outUsername.innerHTML = userValue.value;
-    outPassword.innerHTML = userPass.value;
-}
+btn.onclick = function (e) {
+  e.preventDefault();
+  console.log("pirvet");
+  outUsername.innerHTML = userValue.value;
+  outPassword.innerHTML = userPass.value;
+};
+
+const link = document.querySelector("#link");
+link.onclick = function (e) {
+  e.preventDefault();
+  console.log(e);
+  console.log("pirvet");
+};
 
 //2. получить значение из input  когда нажимается кнопка Add task вывести значение в консоль
 const taskValue = document.querySelector("input[name='task']");
-const taskBtn = document.querySelector('#btn');
-const inputValue = taskValue.value;
+const taskBtn = document.querySelector("#btn");
 
 //  2.1 добавить значние inputa в массив todoList как объект в объекте должно быть два свойства task и id (id должен быть рандомный  Math.random())
-    const todoList =[];
-    taskBtn.onclick = function(){
-        if (inputValue) {
-            const newTask = {
-                task:taskValue,
-                id:Math.random()
-            };
-            todoList.push(newTask);
-        }
-    }
+const todoList = [];
+taskBtn.onclick = function (s) {
+  const inputValue = taskValue.value;
+  s.preventDefault();
+  if (inputValue) {
+    const newTask = {
+      task: inputValue,
+      id: Math.random(),
+    };
+    todoList.push(newTask);
+  }
+};
 //  2.2 вывести массив todoList в консоль
 console.log(todoList);
 // 3  Вывести массив todoList в ul как li поле task
